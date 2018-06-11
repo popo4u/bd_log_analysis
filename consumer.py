624f99e136476f6558c027b508a844a84f1b7177
loyal <furiedboy@gmail.com>
2018-06-11 11:57:00 +0800

    use threading to replace gevent

--- consumer.py
+++ consumer.py
@@ -10,14 +10,16 @@ import yaml
 import collections
 import logging.config
 import confluent_kafka
+import simplejson as josn
 import mysql.connector as DB
 
-from pprint import pprint
 
 
 
-import gevent
-from gevent import monkey; monkey.patch_all()
+# import gevent
+# from gevent import monkey; monkey.patch_all()
+
+import threading
 
 def setup_logging(cfg_path='logging.yaml', level=logging.INFO, env_key='LOG_CFG'):
     """Setup the logging configuration"""
@@ -32,45 +34,6 @@ def setup_logging(cfg_path='logging.yaml', level=logging.INFO, env_key='LOG_CFG'
 setup_logging()
 logger = logging.getLogger('dev')
 
-def test():
-    with open('consumer_conf.yaml') as f:
-        info = yaml.safe_load(f.read())
-        conf = info.get('kafka_info')
-        conn = conf.get('conn')
-        topics = conf.get('topics')
-
-    group = topic.split('__')[0] + '_unibot_status_test'
-    conn['group.id'] = group
-    c = confluent_kafka.Consumer(**conn)
-    c.subscribe(stopics)
-
-    numOfRecords = 2
-
-    try:
-        while numOfRecords > 0:
-            msg = c.poll(timeout=1.0)
-            if msg is None:
-                continue
-            if msg.error():
-                if msg.error().code(
-                ) == confluent_kafka.KafkaError._PARTITION_EOF:
-                    sys.stderr.write(
-                        '%s [%d] reached end at offset %d\n' %
-                        (msg.topic(), msg.partition(), msg.offset()))
-                elif msg.error():
-                    raise confluent_kafka.KafkaException(msg.error())
-            else:
-                print msg
-                print dir(msg)
-                sys.stderr.write('parition: %d, offset: %d, message: %s\n' %
-                                    (msg.partition(), msg.offset(), msg.value()))
-            numOfRecords = numOfRecords - 1
-    except KeyboardInterrupt:
-        sys.stderr.write('Aborted by user\n')
-
-    # Close down consumer to commit final offsets.
-    c.close()
-
 
 class ObjectDict(dict):
     def __inti__(self, *args, **kwargs):
@@ -84,64 +47,51 @@ class ObjectDict(dict):
 
 
 class ConfigHandler(object):
-    def __init__(self, conf_path):
-        self._path = conf_path
+    def __init__(self, cfg_path):
+        self._path = cfg_path
         self.loads()
 
     def __iter__(self):
-        for attr in self.__dict__:
-            if attr.startswith('_'):
-                continue
-            value = self.__dict__[attr]
-            if isinstance(value, collections.Mapping):
-                value = ObjectDict(value)
-            yield value
+        return self._cfg.__iter__()
+
+    def __getattr__(self, name):
+        value = self._cfg[name]
+        if isinstance(value, collections.Mapping):
+            value = ObjectDict(value)
+        return value
+
+    def __getitem__(self, key):
+        return self._cfg.__getitem__(key)
 
     @property
     def _path(self):
-        return self._conf_path
+        return self._cfg_path
 
     @_path.setter
-    def _path(self, conf_path):
-        print conf_path
-        if not os.path.isfile(conf_path):
-            raise IOError, 'The specified %s is not exists!' % conf_path
-        self._conf_path = conf_path
+    def _path(self, cfg_path):
+        if not os.path.isfile(cfg_path):
+            raise IOError, 'The specified path(%s) is not exists!' % cfg_path
+        self._cfg_path = cfg_path
+
+    @property
+    def _ori_data():
+        return self._cfg
+
+    @_ori_data.setter
+    def _ori_data(self, data):
+        if not isinstance(data, collections.Mapping):
+            raise TypeError, 'Data from file %s should be a dict' % self._cfg_path
+        self._cfg = data
 
     def loads(self):
         with open(self._path) as f:
-            conf = yaml.safe_load(f.read())
-        if not isinstance(conf, collections.Mapping):
-            raise TypeError, 'Conf should be a dict'
-        self.extract_cfg(conf)
-
-    def extract_cfg(self, conf):
-        for k, v in conf.items():
-            if isinstance(v, collections.Mapping):
-                v = ObjectDict(v)
-            self.__dict__[k] = v
+            cfg = yaml.safe_load(f.read())
+        self._ori_data = cfg
 
 
 class DBHandler(object):
-    """
-    DB class
-    """
     
     def __init__(self, conf):
-        """
-        init db
-        """
-        # try:
-        #     self.conn = MySQLdb.connect(conf.db_host, conf.db_user, 
-        #             conf.db_pass, conf.db_name, charset = 'utf8')
-        #     self.cursor = self.conn.cursor()
-            
-        #     logger.info('DB params: ' + conf.db_host + ' ' + conf.db_user + ' ' + \
-        #             conf.db_pass + ' ' + conf.db_name)
-        #     logger.info('Connect DB successed !')
-        # except:
-        #     logger.error('Connect DB error !')
-
         try:
             self.conn = DB.connect(**conf)
             self.cursor = self.conn.cursor()
@@ -202,6 +152,8 @@ class SqlBuilder(object):
              '(instance_id, bot_id, bot_model_version, bot_status, create_time, description) bvalues '
              '{instance_id}, {bot_id}, {bot_model_version}, {bot_status}, {create_time}, {description}')
 
+    check_failed_journal = 'Params illegal, %s not exist!, params: \n==> %s'
+
     def __init__(self):
         pass
 
@@ -220,7 +172,7 @@ class SqlBuilder(object):
         if not checked_ok:
             return None
 
-        sql = self.template.format(**params)
+        sql = self.query.format(**params)
         logger.info('Build sql: %s' % sql)
         
         return sql
@@ -230,126 +182,117 @@ class SqlBuilder(object):
         {"bot_status":"loading","instance_id":1137}
         {"bot_id":2148,"bot_model_version":3,"bot_status":"running","instance_id":1137}
         """
-        
-        logger.info('Before checking and fixing params: ' + str(params))
 
-        if 'instance_id' not in params:
-            logger.warn('Params illegal, instance_id is not exist !')
-            return False, params
+        _params = self.QueryParams._make(params.split(','))
+        self.ori_params = copy.deepcopy(_params)
 
-        if 'bot_status' not in params:
-            logger.warn('Params illegal, bot_status is not exist !')
-            return False, params
+        logger.debug('Before checking and fixing params: \n==> %s', json.dumps(_params))
 
-        if 'bot_id' not in params:
-            params['bot_id'] = 0
-        if 'bot_model_version' not in params:
-            params['bot_model_version'] = 3
+        if 'instance_id' not in _params:
+            self.check_failed('instance_id')
+            return False, self.ori_params
+
+        if 'bot_status' not in _params:
+            self.check_failed('bot_status')
+            return False, self.ori_params
+
+        if 'bot_id' not in _params:
+            _params['bot_id'] = 0
+        if 'bot_model_version' not in _params:
+            _params['bot_model_version'] = 3
         time_format = '%Y-%m-%d %H:%M:%S.%f'
-        if 'create_time' not in params:
-            params['create_time'] = datetime.now().strftime(time_format)
-        if 'description' not in params:
-            params['description'] = 'no need desc!'
-        logger.info('After fixing: ')
-        logger.info(params)
+        if 'create_time' not in _params:
+            _params['create_time'] = datetime.now().strftime(time_format)
+        if 'description' not in _params:
+            _params['description'] = 'no need desc!'
 
-        return True, params
+        logger.debug('After fixing params: \n==> %s', json.dumps(_params))
+
+        return True, _params
+
+    def check_failed(self, key):
+        journal = 'Params illegal, %s not exist!, params: \n==> %s'
+        logger.warn(journal, key), json.dumps(self.ori_params)
 
 
 class Consumer(object):
     def __init__(self, conf):
         self.conf = conf
         self.init_kafka()
-        self.init_sql_bulider()
-        self.init_mysql_executer()
+        self.sqlbulider = SqlBuilder()
+        self.db = DBHandler(conf.mysql_info)
 
+        self.continue_run = True
 
     def init_kafka(self):
         conn_info = self.conf.kafka_info.conn
         self.c = confluent_kafka.Consumer(**conn_info)
 
         topics = self.conf.kafka_info.topics
-        print topics
         self.c.subscribe(topics)
 
-    def init_sql_bulider(self):
-        self.sqlbulider = SqlBuilder()
 
-    def init_mysql_executer(self):
-        self.db = DBHandler(self.conf.mysql_info)
-
-    def self_consume(self, _id):
-        while True:
-            print '%s.0' % _id
-            msg = self.c.poll(timeout=1.0)
-            if msg is None:
-                continue
-            if msg.error():
-                if msg.error().code() == confluent_kafka.KafkaError._PARTITION_EOF:
-                    logger.info('partion(%d) => toppic(%s) reached end at offset %d', msg.partition(), msg.topic(), msg.offset())
-                elif msg.error():
-                    logger.exception('Kafka Error: %s', msg.error())
-                continue
-            print '%s.1' % _id
-            data = msg.value()
-            logger.info(data)
-            query = data
-            print 'data: ', data
-            # query = self.sqlbulider.build(data)
-            print '%s.2' %_id
-            if query is not None:
-                self.db.exe(query)
-                print 'exe sql'
-            print '%s.3' % _id
-
-    def consume(self):
-        while True:
-            print '1'
-            msg = self.c.poll(timeout=1.0)
-            if msg is None:
-                continue
-            if msg.error():
-                if msg.error().code() == confluent_kafka.KafkaError._PARTITION_EOF:
-                    logger.info('partion(%d) => toppic(%s) reached end at offset %d', msg.partition(), msg.topic(), msg.offset())
-                elif msg.error():
-                    logger.exception('Kafka Error: %s', msg.error())
-                continue
-            logger.info(msg.value())
-            print '1.1'
-            print '1.2'
-            # self.handler.exe(msg)
+    def run(self, _id):
+        try:
+            while self.continue_run:
+                logger.debug('(%s)consumer waiting for msg', _id)
+                msg = self.c.poll(timeout=1.0)
+                if msg is None:
+                    continue
+                if msg.error():
+                    if msg.error().code() == confluent_kafka.KafkaError._PARTITION_EOF:
+                        logger.info('toppic(%s) [partition(%d)] Reached end at offset %d', msg.topic(), msg.partition(), msg.offset())
+                    else:
+                        logger.exception('Kafka Error: %s', msg.error())
+                    continue
+                data = msg.value()
+                logger.info(data)
+                query = data
+                # query = self.sqlbulider.build(data)
+                if query is not None:
+                    self.db.exe(query)
+                    logger.debug('%s exe sql', _id)
+        except:
+            raise
+        finally:
+            self.close()
 
     def consume_many():
         pass
 
+
+    def terminate(self):
+        self.continue_run = False
+    
     def close(self):
         self.c.close()
-            
-    def set_mysql_handler(self, handler):
-        self.mysql_handler = handler
+        self.db.close()   
 
     def test(self):
-        print '0'
         numOfRecords = 2
         try:
-            while numOfRecords > 0:
-                msg = self.c.poll(timeout=1.0)
-                if msg is None:
-                    print 'msg is none'
-                    continue
-                if msg.error():
-                    if msg.error().code(
-                    ) == confluent_kafka.KafkaError._PARTITION_EOF:
-                        sys.stderr.write(
-                            '%s [%d] reached end at offset %d\n' %
-                            (msg.topic(), msg.partition(), msg.offset()))
-                    elif msg.error():
-                        raise confluent_kafka.KafkaException(msg.error())
-                else:
-                    print msg
-                    print dir(msg)
-                    sys.stderr.write('parition: %d, offset: %d, message: %s\n' %
-                                        (msg.partition(), msg.offset(), msg.value()))
+            while numOfRecords > 0 or 1:
+                logger.debug('start fetching msgs')
+                msgs = self.c.consume(num_messages=3, timeout=5.0)
+                for msg in msgs:
+                    logger.debug('got %s msg', len(msgs))
+                    if msg is None:
+                        logger.debug('msg is none')
+                        continue
+                    if msg.error():
+                        if msg.error().code(
+                        ) == confluent_kafka.KafkaError._PARTITION_EOF:
+                            sys.stderr.write(
+                                '%s [%d] reached end at offset %d\n' %
+                                (msg.topic(), msg.partition(), msg.offset()))
+                        elif msg.error():
+                            raise confluent_kafka.KafkaException(msg.error())
+                    else:
+                        logger.debug('type: %s', type(msg.value()))
+                        logger.debug('value: %s' , msg.value())
+                        # logger.debug(msg.__dict__)
+                        sys.stderr.write('parition: %d, offset: %d, message: %s\n' %
+                                            (msg.partition(), msg.offset(), msg.value()))
                 numOfRecords = numOfRecords - 1
         except KeyboardInterrupt:
             sys.stderr.write('Aborted by user\n')
@@ -392,7 +335,7 @@ def SqlConsumer(cfg):
     return consume_and_exc
 
 
-def run_0(cfg_path):
+def gevent_run(cfg_path):
     cfg = ConfigHandler(cfg_path)
     consume_and_exc = SqlConsumer(cfg)
 
@@ -400,14 +343,33 @@ def run_0(cfg_path):
     g.join()
 
 
-def run_1(cfg_path):
+def run(cfg_path):
     cfg = ConfigHandler(cfg_path)
-    consumer1 = Consumer(cfg)
-    consumer2 = Consumer(cfg)
-    g_1 = gevent.spawn(consumer1.self_consume, 1)
-    g_2 = gevent.spawn(consumer2.self_consume, 2)
-    gevent.joinall([g_1, g_2])
-    # gevent.joinall(map(gevent.spawn, [consumer1.self_consume, consumer2.self_consume]))
+    consumer_num = cfg.kafka_info.partition_num
+    consumers = [ Consumer(cfg) for i in range(consumer_num) ]
+
+    
+    # consumer1 = Consumer(cfg)
+    # consumer2 = Consumer(cfg)
+    # g_1 = gevent.spawn(consumer1.run, 1)
+    # g_2 = gevent.spawn(consumer2.run, 2)
+    # gevent.joinall([g_1, g_2])
+    # gevent.joinall(map(gevent.spawn, [consumer1.run, consumer2.run]))
+    try:
+        thread_list = []
+        for i, c in enumerate(consumers, 1):
+            # thread_list.append(threading.Thread(target=c.run, args=(i, )))
+            t = threading.Thread(target=c.run, args=(i, ))
+            t.daemon = True
+            thread_list.append(t)
+
+        map(lambda t: t.start(), thread_list)
+        map(lambda t: t.join(), thread_list)
+        logger.info('never reach the end of join')
+    except KeyboardInterrupt:
+        logger.info('this will never happen!')
+        map(lambda c: c.terminate(), consumers)
+        raise
 
 
 def test_confighandler():
@@ -418,6 +380,8 @@ def test_confighandler():
     # test if conf is a iterator
     for k in conf:
         print 'k :\n %s' % k
+    print type(conf['mysql_info']), conf['mysql_info']
+    print type(conf.mysql_info), conf.mysql_info
 
 def test_logging():
     setup_logging()
@@ -427,13 +391,13 @@ def test_logging():
 if __name__ == '__main__':
 
     cfg_path = 'consumer.yaml'
-    # test()
-    # c = Consumer('consumer.yaml')
-    # c.test()
+    cfg = ConfigHandler(cfg_path)
+    c = Consumer(cfg)
+    c.test()
     # test_logging()
     # test_confighandler()
-    # run_0(cfg_path)
-    run_1(cfg_path)
+    # gevent_run(cfg_path)
+    # run(cfg_path)
 
 
 
